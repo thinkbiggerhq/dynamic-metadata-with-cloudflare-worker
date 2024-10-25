e291ad89720003b21bc1ae94c0cbd4fa525b8e5d
Think Bigger <alessandro@thinkbigger.it>
2024-10-25 12:39:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://eventi.quivo.it/",
   patterns: [
       {
-          pattern: "/event/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
-      },
-      {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
-      }
+          pattern: "/[^/]+",
+          metaDataEndpoint: "https://sysazaxpkaruieidjojq.supabase.co/rest/v1/events?select=title,meta_description,image,meta_keywords,created_at,code&code=eq.{code}"
+      }      
       // Add more patterns and their metadata endpoints as needed
   ]
 };
